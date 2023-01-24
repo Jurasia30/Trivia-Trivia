a352574dc5d25cd5a6f97fe9baf4a8a6ccfaa32c
Zekerijah Halilovic <zhalilovic02@gmail.com>
2023-01-24 13:56:07 -0500

    added scores

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 
+
+
+
+const easyQuestions = document.querySelectorAll("[id$='easy']")
+
+easyQuestions.forEach(question => {
+    question.textContent = 100;
+})
+
+const mediumQuestions = document.querySelectorAll("[id$='medium']")
+
+mediumQuestions.forEach(question => {
+    question.textContent = 200;
+})
+
+
+const mediumQuestionsTwo = document.querySelectorAll("[id$='medium-2']")
+
+mediumQuestionsTwo.forEach(question => {
+    question.textContent = 300;
+})
+
+const hardQuestions = document.querySelectorAll("[id$='hard']")
+
+hardQuestions.forEach(question => {
+    question.textContent = 300;
+})
+
